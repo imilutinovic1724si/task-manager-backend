68f2d1fc8f9356da05095eef1b257c43f4f6869a
Iva Milutinovic <imilutinovic1724si@raf.rs>
2026-08-31 10:50:09 +0200

    Dodaj healt check rutu za keep-alive

--- src/main/java/com/iva/task_manager/config/SecurityConfig.java
+++ src/main/java/com/iva/task_manager/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(csrf ->csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-                        .requestMatchers("/api/auth/**", "/error").permitAll()
+                        .requestMatchers("/api/auth/**", "/error", "/api/health").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
---
+++ src/main/java/com/iva/task_manager/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.iva.task_manager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "OK";
+    }
+}
